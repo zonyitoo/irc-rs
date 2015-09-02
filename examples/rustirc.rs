2b29f7a45cbd8d41179b8752cc58bf621284ddec
Y. T. Chung <zonyitoo@gmail.com>
2015-09-02 17:48:41 +0800

    added command line argument, fixed bugs

--- examples/rustirc.rs
+++ examples/rustirc.rs
@@ -4,28 +4,31 @@ extern crate env_logger;
 extern crate irc;
 
 use std::net::TcpStream;
-use std::io::{Write, BufRead, BufReader};
+use std::io::{self, Write, BufRead, BufReader};
+use std::thread;
+use std::env;
 
 use irc::protocol::message::{IrcMessage, Message, Body};
-use irc::protocol::command::{Command, UserCommand, NickCommand, JoinCommand, PongCommand};
+use irc::protocol::command::{Command, UserCommand, NickCommand, JoinCommand, PongCommand, PrivmsgCommand, QuitCommand};
 use irc::protocol::reply::ReplyCode;
 
 fn main() {
     env_logger::init().unwrap();
 
+    let username: String = env::args().skip(1).next().expect("You have to provide a username").clone();
     let server_addr = "irc.mozilla.org:6667";
+
+    println!("Logging to {:?} as {:?}", server_addr, username);
 
     info!("Connecting to {:?}", server_addr);
     let mut stream = TcpStream::connect(server_addr).unwrap();
     let bufreader = BufReader::new(stream.try_clone().unwrap());
 
-    let username = "rust_irc_test";
-    let nickname = username;
-
-    let usercmd = UserCommand::new(username, 0, username);
-    let nickcmd = NickCommand::new(nickname);
+    let nickname = &username[..];
 
     {
+        let usercmd = UserCommand::new(&username[..], 0, &username[..]);
+        let nickcmd = NickCommand::new(nickname);
         let usermsg = Message::new(None, Body::command(usercmd));
         let nickmsg = Message::new(None, Body::command(nickcmd));
 
@@ -51,6 +54,12 @@ fn main() {
                     let params = rpl.parameters().collect::<Vec<&str>>();
                     println!("-*- {}", params.connect(" "));
                     break;
+                } else if let ReplyCode::ERR_NICKNAMEINUSE = rpl.code() {
+                    panic!("Nickname {:?} already in use", nickname);
+                }
+            } else if let &Body::Command(ref cmd) = parsed.body() {
+                if let &Command::Notice(ref notice) = cmd {
+                    println!("-*- Notice: {}", notice.message());
                 }
             }
         }
@@ -87,6 +96,42 @@ fn main() {
         info!("Join channel: Sending {:?}", joinmsg);
         write!(stream, "{}\r\n", joinmsg).unwrap();
     }
+
+    let mut writeable_stream = stream.try_clone().unwrap();
+    let cloned_username = username.clone();
+    thread::spawn(move|| {
+        let bufstdin = BufReader::new(io::stdin());
+        for line in bufstdin.lines() {
+            let line = line.unwrap();
+            let msg = line.trim();
+
+            if !msg.is_empty() {
+                if msg.starts_with("/") {
+                    let mut sp = msg.splitn(2, ' ');
+                    let cmd = sp.next().unwrap();
+                    let cmd = (&cmd[1..]).chars().flat_map(|x| x.to_uppercase()).collect::<String>();
+
+                    match &cmd[..] {
+                        "QUIT" => {
+                            let quitcmd = QuitCommand::new(sp.next());
+                            let msg = Message::new(None, Body::command(quitcmd));
+                            trace!("Quit: Sending {:?}", msg);
+                            write!(writeable_stream, "{}\r\n", msg).unwrap();
+                        },
+                        _ => {
+                            error!("Unknown command {}", cmd);
+                        }
+                    }
+                } else {
+                    let privmsg = PrivmsgCommand::new("#rust", msg);
+                    println!("-*- < {} > {}", cloned_username, msg);
+                    let msg = Message::new(None, Body::command(privmsg));
+                    trace!("Privmsg: Sending {:?}", msg);
+                    write!(writeable_stream, "{}\r\n", msg).unwrap();
+                }
+            }
+        }
+    });
 
     // Prints conversation
     for line in lines {
